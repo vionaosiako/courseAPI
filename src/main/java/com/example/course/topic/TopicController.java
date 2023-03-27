2b4888ea44fd24c1fe50a8623212d4a9ce22df9d
vionaosiako <viona.osiako@student.moringaschool.com>
2023-03-27 20:04:29 +0300

    create a list

--- src/main/java/com/example/course/topic/TopicController.java
+++ src/main/java/com/example/course/topic/TopicController.java
@@ -3,10 +3,23 @@ package com.example.course.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
+//    @RequestMapping("/topics")
+//    public String GetAllTopics() {
+//        return "All topics";
+//    }
     @RequestMapping("/topics")
-    public String GetAllTopics() {
-        return "All topics";
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+            new Topic("spring","spring framework","spring framework description"),
+            new Topic("java","core java","java description"),
+            new Topic("javascript","javascript framework","javascript framework description")
+
+
+        );
     }
 }
