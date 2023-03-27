74f34b2920b7a23d5f1d0cfafee95cf9198a7838
vionaosiako <viona.osiako@student.moringaschool.com>
2023-03-27 19:13:56 +0300

    create getter and setters

--- src/main/java/com/example/course/topic/Topic.java
+++ src/main/java/com/example/course/topic/Topic.java
@@ -1,4 +1,33 @@
 package com.example.course.topic;
 
 public class Topic {
+    private  String id;
+    private String name;
+    private String description;
+
+    // getter and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
 }
