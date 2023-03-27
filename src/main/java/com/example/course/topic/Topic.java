8344085f02901a59f44c98099f4c35afcae35e35
vionaosiako <viona.osiako@student.moringaschool.com>
2023-03-27 19:15:03 +0300

    create contractor

--- src/main/java/com/example/course/topic/Topic.java
+++ src/main/java/com/example/course/topic/Topic.java
@@ -5,6 +5,17 @@ public class Topic {
     private String name;
     private String description;
 
+    //contractor
+    public Topic() {
+
+    }
+
+    public Topic(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
     // getter and setters
     public String getId() {
         return id;
