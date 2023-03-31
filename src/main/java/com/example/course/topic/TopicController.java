82f61051fe8702d8402d43993401eefab0c65fc9
vionaosiako <viona.osiako@student.moringaschool.com>
2023-03-31 21:50:25 +0300

    Create POST request method in the TopicController

--- src/main/java/com/example/course/topic/TopicController.java
+++ src/main/java/com/example/course/topic/TopicController.java
@@ -1,9 +1,7 @@
 package com.example.course.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -34,5 +32,10 @@ public class TopicController {
     @RequestMapping("/topics/{id}")
     public Topic getTopic(@PathVariable String id){
         return topicService.getTopic(id);
+    }
+
+//    @RequestMapping(method = RequestMethod.POST, value ="/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
     }
 }
--- src/main/java/com/example/course/topic/TopicService.java
+++ src/main/java/com/example/course/topic/TopicService.java
@@ -21,4 +21,5 @@ public class TopicService {
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
     }
 
+
 }
